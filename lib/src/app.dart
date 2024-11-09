daf7fa6cd809c08361d14d485732dddff9f784cb
Bilawal-Mehfooz-Malik <bilawalmehfoozflutter@gmail.com>
2024-11-09 17:18:45 +0500

    added all data

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
+import 'package:ecommerce_app/src/routing/app_router.dart';
+import 'package:ecommerce_app/src/themes/light_theme.dart';
+
+class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final goRouter = ref.watch(goRouterProvider);
+
+    return MaterialApp.router(
+      routerConfig: goRouter,
+      theme: flexColorScheme,
+      restorationScopeId: 'app',
+      debugShowCheckedModeBanner: false,
+      onGenerateTitle: (BuildContext context) => 'My Shop'.hardcoded,
+    );
+  }
+}
