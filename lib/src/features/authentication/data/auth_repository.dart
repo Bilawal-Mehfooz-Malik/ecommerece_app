db350527c11b3ed9a1e53fc1483858fd68fc1b8a
Bilawal-Mehfooz-Malik <bilawalmehfoozflutter@gmail.com>
2024-11-13 20:28:23 +0500

    added firebase auth repository

--- lib/src/features/authentication/data/auth_repository.dart
+++ lib/src/features/authentication/data/auth_repository.dart
@@ -1,29 +1,57 @@
 import 'dart:async';
 
-import 'package:ecommerce_app/src/features/authentication/domain/app_user.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import 'package:ecommerce_app/src/features/authentication/domain/app_user.dart';
 
 part 'auth_repository.g.dart';
 
-// TODO: Implement with Firebase
-abstract class AuthRepository {
-  Future<void> signInWithEmailAndPassword(String email, String password);
-  Future<void> createUserWithEmailAndPassword(String email, String password);
-  Future<void> signOut();
-  Stream<AppUser?> authStateChanges();
-  AppUser? get currentUser;
+class AuthRepository {
+  AuthRepository(this._auth);
+
+  final FirebaseAuth _auth;
+
+  Future<void> signInWithEmailAndPassword(String email, String password) {
+    return _auth.signInWithEmailAndPassword(email: email, password: password);
+  }
+
+  Future<void> createUserWithEmailAndPassword(String email, String password) {
+    return _auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  Future<void> signOut() {
+    return _auth.signOut();
+  }
+
+  Stream<AppUser?> authStateChanges() {
+    return _auth.authStateChanges().map(_convertUser);
+  }
+
+  AppUser? get currentUser => _convertUser(_auth.currentUser);
+
+  AppUser? _convertUser(User? user) => user != null
+      ? AppUser(
+          uid: user.uid,
+          email: user.email,
+          emailVerified: user.emailVerified,
+        )
+      : null;
 }
 
 @Riverpod(keepAlive: true)
-AuthRepository authRepository(AuthRepositoryRef ref) {
-  // TODO: create and return repository
-  throw UnimplementedError();
+AuthRepository authRepository(Ref ref) {
+  return AuthRepository(FirebaseAuth.instance);
 }
 
 // * Using keepAlive since other providers need it to be an
 // * [AlwaysAliveProviderListenable]
 @Riverpod(keepAlive: true)
-Stream<AppUser?> authStateChanges(AuthStateChangesRef ref) {
+Stream<AppUser?> authStateChanges(Ref ref) {
   final authRepository = ref.watch(authRepositoryProvider);
   return authRepository.authStateChanges();
 }
